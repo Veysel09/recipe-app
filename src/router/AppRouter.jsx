6b2901a6d9cfc99c4e97479a0b5f3fdebe90291e
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-26 12:21:04 +0300

    router

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./../components/nav/Navbar";
+import Home from "../pages/home/Home";
+import Login from "./../pages/login/Login";
+import About from "../pages/about/About";
+import Detail from "../pages/detail/Detail";
+import PrivateRouter from "./PrivateRouter";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="about" element={<PrivateRouter />}>
+          <Route path="" element={<About />} />
+        </Route>
+        <Route path="detail" element={<PrivateRouter />}>
+          <Route path="" element={<Detail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
