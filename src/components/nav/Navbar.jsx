e5e0e644d4e9480dae7ce7d23337f7f7a9618084
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-28 11:14:33 +0300

    logout

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import "./Navbar.style";
 import Nav, { Brand, Menu, MenuLink, Hamburger } from "./Navbar.style";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
   return (
     <Nav justify="space-between" wrap="wrap">
@@ -20,7 +22,10 @@ const Navbar = () => {
         <MenuLink to="/">Home</MenuLink>
         <MenuLink to="about">About</MenuLink>
         <MenuLink to="register">Register</MenuLink>
-        <MenuLink to="Logout" onClick={() => sessionStorage.clear()}>
+        <MenuLink
+          to="Logout"
+          onClick={() => sessionStorage.clear() && navigate("/login")}
+        >
           Logout
         </MenuLink>
       </Menu>
@@ -29,4 +34,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-  
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Header } from "../login/Login.style";
 
 const Home = () => {
@@ -16,9 +16,9 @@ const Home = () => {
     setRecipes(data.hits);
     // console.log(recipes);
   };
-  useEffect(() => {
-    getData();
-  }, []);
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
   return (
     <div>
