16919893b94fe36d88cf554135e1bc5d5accf925
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-01 21:20:49 +0300

    about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
+import Header from "../../components/header/Header"
 
 const About = () => {
   return (
     <div>
+      <Header/>
         About
-        <Outlet/>
-        </div>
+      <Outlet/>
+    </div>
   )
 }
 
