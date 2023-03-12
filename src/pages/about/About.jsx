6608d27d0ad9f4606357dea1e816d4d5fb310ec5
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-12 21:25:13 +0300

    about span

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -6,11 +6,15 @@ import AboutImg from "../../assets/a.jpg"
 const About = () => {
   return (
     <AboutContainer>
-    
-        <Image src={AboutImg} alt="" />
-    
+      <Image src={AboutImg} alt="" />
 
-      <AboutText>	The aim of the Project: Creating a react project styled with styled component logic with login/logout feature. It is necessary to log in to enter the food details and see the about us page. Skills/Tools: React-Router | Styled Component | axios |async/await</AboutText>
+      <AboutText>
+        {" "}
+        <span>The aim of the Project</span>: Creating a react project styled
+        with styled component logic with login/logout feature. It is necessary
+        to log in to enter the food details and see the about us page.
+        Skills/Tools: React-Router | Styled Component | axios |async/await
+      </AboutText>
     </AboutContainer>
   );
 };
--- src/pages/about/About.style.jsx
+++ src/pages/about/About.style.jsx
@@ -1,7 +1,4 @@
 import styled from "styled-components";
-import Flex from "../../components/globalStyles/Flex.style";
-
-
 export const AboutContainer = styled.div`
   background: ${({ theme }) => theme.colors.mainColor};
   justify-content: space-between;
